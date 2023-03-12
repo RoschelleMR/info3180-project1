50e8c1e13bbb6f88de0718a4a7860d1eb34371db
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2023-03-12 00:38:27 -0500

    Updated forms.py

--- app/forms.py
+++ app/forms.py
@@ -6,10 +6,10 @@ from flask_wtf.file import FileAllowed, FileRequired
 class PropertyForm(FlaskForm):
     title = StringField('Property Title', validators=[InputRequired()])
     desc = TextAreaField("Description", validators=[InputRequired()])
-    rooms = IntegerField("No. of Rooms", validators=[InputRequired()])
+    bedrooms = IntegerField("No. of Bedrooms", validators=[InputRequired()])
     bathrooms = IntegerField("No. of Bathrooms", validators=[InputRequired()])
     price = IntegerField("Price", validators=[InputRequired()])
-    type = SelectField('Property Type', choices=[('house', 'House'), ('apart', 'Apartment')])
+    type = SelectField('Property Type', choices=[('house', 'House'), ('apartment', 'Apartment')])
     location = StringField('Location', validators=[InputRequired()])
     photo = FileField("Photo", validators=[InputRequired(), FileRequired(), FileAllowed(['jpg','jpeg', 'jfif', 'png', 'gif'], 'Images only')])
     submit = SubmitField("Add Property")
