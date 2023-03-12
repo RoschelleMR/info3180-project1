52e2ce16f9066d9972870b1ea0c76342ccc5a569
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2023-03-12 00:37:57 -0500

    Chnaged the size of columns

--- app/models.py
+++ app/models.py
@@ -6,18 +6,18 @@ class PropertyProfile(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(180))
-    desc = db.Column(db.String(180))
+    desc = db.Column(db.String(250))
     bedrooms = db.Column(db.Integer)
     bathrooms = db.Column(db.Integer)
-    price = db.Column(db.String(180))
+    price = db.Column(db.String(80))
     property_type = db.Column(db.String(80))
     location = db.Column(db.String(180))
-    photo_filename = db.Column(db.String(180))
+    photo_filename = db.Column(db.String(80))
     
-    def __init__(self, title, desc, rooms, bathrooms, price, prop_type, location, photo_filename):
+    def __init__(self, title, desc, bedrooms, bathrooms, price, prop_type, location, photo_filename):
         self.title = title
         self.desc = desc
-        self.bedrooms = rooms
+        self.bedrooms = bedrooms
         self.bathrooms = bathrooms
         self.price = price
         self.property_type = prop_type
